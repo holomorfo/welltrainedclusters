3555b2b5f0d3b85dfa5f987ac376b9b3d9516321
Cristian Banuelos <holomorfo.mx@gmail.com>
2019-10-01 12:35:09 -0700

    Update

--- extact.py
+++ extact.py
@@ -4,6 +4,4 @@ import simplemir.music21utils as mu
 path = 'wtcbki'
 
 pathList = fu.get_list_files(path, extension="mid")
-scores = mu.get_scores_from_paths(pathList)
-print(scores[1][1])
-scores[1][1].show('txt')
+scoreTuppleList = mu.get_scores_from_paths(pathList[:2])
